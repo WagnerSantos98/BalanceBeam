89841cd2d268ddd3fde804a0910e71f5a2e079ce
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-08-21 18:58:22 -0300

    refactor: alteração ede parametros no script

---
+++ assets/js/questions.js
@@ -0,0 +1,36 @@
+export default [
+    {
+      question: "Quanto é 1 + 2?",
+      answers: [
+        { option: "3", correct: true },
+        { option: "2", correct: false },
+        { option: "4", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 5 x 2?",
+      answers: [
+        { option: "9", correct: false },
+        { option: "10", correct: true },
+        { option: "11", correct: false },
+      ],
+    },
+    {
+      question: "Qual foi a primeira capital do Berasil?",
+      answers: [
+        { option: "Brasília", correct: false },
+        { option: "Rio de Janeiro", correct: false },
+        { option: "Salvador", correct: true },
+        { option: "São Paulo", correct: false },
+      ],
+    },
+    {
+      question: "O Brasil é dividido em quantas regiões?",
+      answers: [
+        { option: "3", correct: false },
+        { option: "2", correct: false },
+        { option: "7", correct: false },
+        { option: "5", correct: true },
+      ],
+    },
+  ];
